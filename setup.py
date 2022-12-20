7c05be60ce030f7b2e4db0e2ced65f43691631c6
sergey_v <sergey@cohouse.com>
2022-12-20 11:43:07 +0200

    removed references to tmp file during setup

--- setup.py
+++ setup.py
@@ -26,22 +26,22 @@ NAME = "hubspot-api-client"
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 DEV_REQUIRES = ["pytest", "black"]
 
-DIR_PATH = dirname(abspath(__file__))
+# DIR_PATH = dirname(abspath(__file__))
 
-with open(DIR_PATH + "/VERSION", "r", encoding="utf-8") as f:
-    VERSION = f.readline().strip()
+# with open(DIR_PATH + "/VERSION", "r", encoding="utf-8") as f:
+#     VERSION = f.readline().strip()
 
-LONG_DESCRIPTION = None
-if exists(DIR_PATH + "/README.md"):
-    with open(DIR_PATH + "/README.md", "r", encoding="utf-8") as f:
-        LONG_DESCRIPTION = f.read()
+# LONG_DESCRIPTION = None
+# if exists(DIR_PATH + "/README.md"):
+#     with open(DIR_PATH + "/README.md", "r", encoding="utf-8") as f:
+#         LONG_DESCRIPTION = f.read()
 
 setup(
     name=NAME,
     packages=find_packages(),
-    version=VERSION,
+    version='5.1.1',
     description="HubSpot API client",
-    long_description=LONG_DESCRIPTION,
+    long_description="LONG_DESCRIPTION",
     long_description_content_type="text/markdown",
     url="https://github.com/HubSpot/hubspot-api-python",
     author="HubSpot",
